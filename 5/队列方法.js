ddbd74e21c4bc4fbb8845401d5d2c704df808340
JavaScript高级程序设计练习 <630374347@qq.com>
2017-09-14 14:47:14 +0800

    Create 队列方法.js

---
+++ 5/队列方法.js
@@ -0,0 +1,8 @@
+var colors = new Array();
+var count = colors.push("red","green");
+alert(count);
+count = colors.push("black");
+alert(count);
+var item = colors.shift();
+alert(item);
+alert(colors.length);
