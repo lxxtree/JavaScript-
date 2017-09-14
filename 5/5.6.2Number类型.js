888468ede4ae8ef37c4f9a2094afb8e29976ad1c
JavaScript高级程序设计练习 <630374347@qq.com>
2017-09-14 14:25:53 +0800

    Create 5.6.2Number类型.js

---
+++ 5/5.6.2Number类型.js
@@ -0,0 +1,18 @@
+var num = 10;
+alert(num.toString());
+alert(num.toString(2));
+alert(num.toString(8));
+alert(num.toString(10));
+alert(num.toString(16));
+alert(num.toFixed(2));
+
+var num1 = 10.005;
+alert(num1.toFixed(2));
+
+var num2 = 10;
+alert(num2.toExponential(1));
+
+var num3 = 99;
+alert(num3.toPrecision(1));
+alert(num3.toPrecision(2));
+alert(num3.toPrecision(3));
