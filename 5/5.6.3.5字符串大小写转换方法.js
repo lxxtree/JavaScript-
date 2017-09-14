2b695750325201a83f5a9deeddbed559bd8a28cf
JavaScript高级程序设计练习 <630374347@qq.com>
2017-09-14 14:34:04 +0800

    Create 5.6.3.5字符串大小写转换方法.js

---
+++ 5/5.6.3.5字符串大小写转换方法.js
@@ -0,0 +1,6 @@
+//大小写转换方法
+var stringValue = "hello world";
+alert(stringValue.toLocaleUpperCase());
+alert(stringValue.toUpperCase());
+alert(stringValue.toLocaleLowerCase());
+alert(stringValue.toLowerCase());
