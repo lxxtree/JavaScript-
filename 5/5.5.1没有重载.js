82c16b40fdc721928b572a1869e370cfdd6c0d6a
JavaScript高级程序设计练习 <630374347@qq.com>
2017-09-14 14:18:04 +0800

    Create 5.5.1没有重载.js

---
+++ 5/5.5.1没有重载.js
@@ -0,0 +1,17 @@
+function addSomeNumber(num){
+    return num + 100;
+}
+function addSomeNumber(num){
+    return num + 200;
+}
+var result = addSomeNumber(100);
+alert(result);
+
+var addSomeNumber1 = function(num){
+    return num + 100;
+};
+var addSomeNumber1 = function(num){
+    return num + 200;
+};
+var result1 = addSomeNumber(100);
+alert(result1);
