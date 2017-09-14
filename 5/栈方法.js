6f874023a4ea3aaebb3721bfdf7bc0409bc515aa
JavaScript高级程序设计练习 <630374347@qq.com>
2017-09-14 14:48:46 +0800

    Create 栈方法.js

---
+++ 5/栈方法.js
@@ -0,0 +1,8 @@
+var colors = new Array();
+var count = colors.push("red","green");
+alert(count);
+count = colors.push("black");
+alert(count);
+var item = colors.pop();
+alert(item);
+alert(colors.length);
