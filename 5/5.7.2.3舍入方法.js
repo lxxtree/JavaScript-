22d7692c173978a9de21f849691add4d317e7683
JavaScript高级程序设计练习 <630374347@qq.com>
2017-09-14 14:44:51 +0800

    Create 5.7.2.3舍入方法.js

---
+++ 5/5.7.2.3舍入方法.js
@@ -0,0 +1,16 @@
+/*
+* Math.ceil()执行向上舍入，即它总是将数值向上舍入作为最接近的整数
+* Math.floor()执行向下舍入，即它总是将数值向下舍入作为最接近的整数
+* Math.round()执行标准舍入，即它总是将数值四舍五入为最接近的整数
+ */
+alert(Math.ceil(25.9));
+alert(Math.ceil(25.5));
+alert(Math.ceil(25.1));
+
+alert(Math.floor(25.9));
+alert(Math.floor(25.5));
+alert(Math.floor(25.1));
+
+alert(Math.round(25.9));
+alert(Math.round(25.5));
+alert(Math.round(25.1));
